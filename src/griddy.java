8dca792d511484b2e151d2db48dc97676ff408e3
Kashif Smith <smithkashif@gmail.com>
2014-07-17 09:48:48 -0700

    simple print

---
+++ src/griddy.java
@@ -0,0 +1,16 @@
+
+
+/* @author Kashif Smith main method for taking a US patent number and inputting the 
+ *		   correctly formatted data.
+ */
+
+
+
+
+
+
+public class griddy {
+	public static void main (String args[]) {
+		System.out.println("we're here!");
+	}
+}
